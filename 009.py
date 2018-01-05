86c2a0eddf170f1493f6279e319bbe3ae8366a1d
seok yun <rifjvn07@naver.com>
2018-01-05 15:09:47 +0900

    Solution add 009& 010

---
+++ 009.py
@@ -0,0 +1,13 @@
+# 사칙연 +,-,*,/
+
+a = int(input("first number: "))
+b = int(input("second number: "))
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("add", add)
+print("sub", sub)
+print("mul", mul)
+print("div", div)
---
+++ 010.py
@@ -0,0 +1,4 @@
+# 거듭제곱
+a = int(input("밑: "))
+b = int(input("지수: "))
+print(a, "^", b, "=", a ** b)
